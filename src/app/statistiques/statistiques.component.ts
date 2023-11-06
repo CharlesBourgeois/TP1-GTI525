df12d6557bd22a527f6f538c01c272b496a78305
BSimardJulien <julien.b.simard@outlook.com>
2023-11-06 17:30:47 -0500

    KK

--- src/app/statistiques/statistiques.component.ts
+++ src/app/statistiques/statistiques.component.ts
@@ -1,33 +1,164 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { CsvService } from '../csv.service';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-statistiques',
   templateUrl: './statistiques.component.html',
-  styleUrls: ['./statistiques.component.css']
+  styleUrls: ['./statistiques.component.css'],
 })
 export class StatistiquesComponent implements OnInit {
   public records: any[] = [];
   public currentPage: number = 1;
+  public isModalVisible: boolean = false;
+  public selectedPointData: any = null;
   currentSortField: string = 'ID';
   sortDirection: 'asc' | 'desc' = 'asc';
   searchTerm: string = '';
   filteredRecords: any[] = [];
+  map!: mapboxgl.Map;
+  style = 'mapbox://styles/mapbox/streets-v11'; // your style URL
+  lat = 45.5017; // default latitude
+  lng = -73.5673; // default longitude
 
-  constructor(private csvService: CsvService) { }
+  constructor(
+    private csvService: CsvService,
+    private changeDetectorRef: ChangeDetectorRef
+  ) {}
 
   ngOnInit() {
-    this.csvService.loadData('assets/compteurs.csv')
-      .then(data => {
-        this.records = data;
-        this.filteredRecords = [...this.records];
-      });
+    this.csvService.loadData('assets/compteurs.csv').then((data) => {
+      this.records = data;
+      this.filteredRecords = [...this.records];
+    });
   }
 
+  ngAfterViewInit() {
+    this.initializeMap();
+  }
+
+  private convertToGeoJSON(
+    records: any[]
+  ): GeoJSON.FeatureCollection<GeoJSON.Point> {
+    return {
+      type: 'FeatureCollection',
+      features: records.map((record) => ({
+        type: 'Feature',
+        properties: {
+          ID: record.ID,
+          Nom: record.Nom,
+          Statut: record.Statut,
+          Annee_implante: record.Annee_implante,
+        },
+        geometry: {
+          type: 'Point',
+          coordinates: [
+            parseFloat(record.Longitude),
+            parseFloat(record.Latitude),
+          ],
+        },
+      })),
+    };
+  }
+
+  private initializeMap() {
+    if (this.map) return;
+
+    (mapboxgl as any).accessToken =
+      'pk.eyJ1IjoiamJzaW1hcmQiLCJhIjoiY2xvaGV3YnVhMGF1eTJqbzV2a3lzc3FzeCJ9.1arX2YNM65XRaJQB_ewsWA';
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: this.style,
+      zoom: 12,
+      center: [this.lng, this.lat],
+    });
+
+    this.map.on('load', () => {
+      const geojsonData = this.convertToGeoJSON(this.records);
+      this.map.addSource('points', {
+        type: 'geojson',
+        data: geojsonData,
+      });
+      this.map.addLayer({
+        id: 'points',
+        type: 'circle',
+        source: 'points',
+        paint: {
+          'circle-radius': 8,
+          'circle-color': '#007cbf',
+        },
+      });
+
+      const popup = new mapboxgl.Popup({
+        closeButton: false,
+        closeOnClick: false,
+      });
+
+      this.map.on('mouseenter', 'points', (e) => {
+        if (e.features?.length) {
+          this.map.getCanvas().style.cursor = 'pointer';
+
+          const feature = e.features[0];
+          if (feature.geometry.type === 'Point') {
+            const coordinates = feature.geometry.coordinates as [
+              number,
+              number
+            ];
+            const description = feature.properties?.['Nom'] ?? '';
+
+            popup
+              .setLngLat(coordinates)
+              .setHTML(`<strong>${description}</strong>`)
+              .addTo(this.map);
+
+            // Set the hovered feature paint property using the feature's layer and source.
+            this.map.setPaintProperty('points', 'circle-color', [
+              'case',
+              ['==', ['get', 'ID'], feature.properties?.['ID']],
+              '#ff0000',
+              '#007cbf',
+            ]);
+          }
+        }
+      });
+
+      this.map.on('mouseleave', 'points', () => {
+        this.map.getCanvas().style.cursor = '';
+        popup.remove();
+        this.map.setPaintProperty('points', 'circle-color', '#007cbf');
+      });
+
+      this.map.on('click', 'points', (e) => {
+        if (e.features?.length) {
+          const feature = e.features[0];
+          if (feature.geometry.type === 'Point') {
+            this.selectedPointData = {
+              ID: feature.properties?.['ID'],
+              Nom: feature.properties?.['Nom'],
+              Statut: feature.properties?.['Statut'],
+              Annee_implante: feature.properties?.['Annee_implante'],
+            };
+            // Open the modal
+            this.isModalVisible = true;
+            this.changeDetectorRef.detectChanges();
+          }
+        }
+      });
+    });
+  }
+
+  currentView: 'list' | 'map' = 'list';
+
+  toggleView(view: 'list' | 'map'): void {
+    this.currentView = view;
+    if (view === 'map') {
+      setTimeout(() => this.initializeMap(), 0);
+    }
+  }
 
   filterRecords() {
     if (this.searchTerm) {
-      this.filteredRecords = this.records.filter(record =>
+      this.filteredRecords = this.records.filter((record) =>
         record.Nom.toLowerCase().includes(this.searchTerm.toLowerCase())
       );
     } else {
