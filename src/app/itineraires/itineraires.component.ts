564b00c60da60ca7dbcdd4e40bc6652ca4bf858b
Julien Boisvert-Simard <julien.boisvert-simard.1@ens.etsmtl.ca>
2023-12-04 11:08:13 -0500

    T4.6

--- src/app/itineraires/itineraires.component.ts
+++ src/app/itineraires/itineraires.component.ts
@@ -1,20 +1,42 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-itineraires',
   templateUrl: './itineraires.component.html',
-  styleUrls: ['./itineraires.component.css']
+  styleUrls: ['./itineraires.component.css'],
 })
 export class ItinerairesComponent {
-
   activeSeason: string = 'Saisonnier';
   showProtectedPaths: boolean = false;
   showSharedPaths: boolean = false;
+  startDate!: string;
+  endDate!: string;
+  topArrondissements: any;
 
-  constructor() {
+  constructor(private apiService: ApiService) {}
+
+  fetchPopularTracks() {
+    const params = {
+      populaireDebut: this.startDate.split('-').join(''),
+      populaireFin: this.endDate.split('-').join(''),
+    };
+
+    this.apiService
+      .loadDataWithParams('http://localhost:8080/gti525/v1/pistes', params)
+      .subscribe(
+        (data) => {
+          this.topArrondissements = data.topArrondissements;
+        },
+        (error) => {
+          console.error('Error fetching data: ', error);
+        }
+      );
+  }
+  objectKeys(obj: any): string[] {
+    return Object.keys(obj);
   }
 
-  // Function to open the modal
   openModal() {
     const modal = document.getElementById('myModal');
     if (modal) {
@@ -22,7 +44,6 @@ export class ItinerairesComponent {
     }
   }
 
-  // Function to close the modal
   closeModal() {
     const modal = document.getElementById('myModal');
     if (modal) {
